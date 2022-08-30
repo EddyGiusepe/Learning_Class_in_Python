21028534e4b1d2eeec87fe6168929b3db2e41f63
EddyGiusepe <eddychirinos.unac@gmail.com>
2022-08-30 19:22:12 -0300

    More example of POO

--- POO_programador_Lhama/minha_classe.py
+++ POO_programador_Lhama/minha_classe.py
@@ -1,5 +1,7 @@
 '''
 Data Scientist.: Dr.Eddy Giusepe Chirinos Isidro
+
+Link de estudo --> https://www.youtube.com/watch?v=WP5p4QEqLLQ&list=PLAgbpJQADBGLo24x_xBwGtTDO-bjwrFb_&index=1
 '''
 print("=========")
 print("Exemplo 1")
@@ -77,3 +79,53 @@ class MinhaClasse:
 # Instanciando minha classe:
 objeto = MinhaClasse(23)
 objeto.meu_metodo()
+
+
+
+print("==========================")
+print("Exemplo do controle remoto")
+print("==========================")
+class ControleRemoto():
+
+    def __init__(self, televisao, comodo):
+        self.televisao = televisao
+        self.comodo = comodo
+
+    def avancar_canal(self):
+        print('Canal avançado')
+
+    def voltar_canal(self):
+        print('Voltar canal')
+
+    def escolher_canal(self, canal):
+        print("Alterado para o canal: {}".format(canal))
+
+# Na sala:
+controle_sala = ControleRemoto('samsung', 'sala')
+print(controle_sala.comodo)
+print(controle_sala.televisao)
+controle_sala.avancar_canal()
+controle_sala.escolher_canal(12)
+
+print("")
+# No quarto
+controle_quarto = ControleRemoto('LG', 'quarto')
+print(controle_quarto.comodo)
+print(controle_quarto.televisao)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
