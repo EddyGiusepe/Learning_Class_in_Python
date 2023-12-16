c03dc831c1301f1671490a0ea35f09b731c598c9
EddyGiusepe <eddychirinos.unac@gmail.com>
2023-12-15 23:31:49 -0300

    Loop control

---
+++ Simple_Python_Code/2_Loop_Control.py
@@ -0,0 +1,11 @@
+"""
+Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+"""
+for number in range(1, 6):
+    if number == 2:
+        continue
+
+    product = number * 2
+    print(number, "* 2 = ", product)
+
+print("Loop completed")
