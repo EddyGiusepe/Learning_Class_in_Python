9cbcdc83bfee8034cd9e2b799d8e688bdc0720cd
EddyGiusepe <eddychirinos.unac@gmail.com>
2023-12-19 23:16:25 -0300

    Backtracking

--- Simple_Python_Code/11_Backtracking_Algorithm.py
+++ Simple_Python_Code/11_Backtracking_Algorithm.py
@@ -30,7 +30,6 @@ def subsets(nums: list[int]) -> list[list[int]]:
 
 
 
-
 if __name__ == '__main__':
     array = [3, 1, 2] # Resposta será: [[], [2], [1], [1, 2], [3], [3, 2], [3, 1], [3, 1, 2]]
     response = subsets(array)
