8e8a5f23a805e84bfded9745b5238d84ca4e9940
EddyGiusepe <eddychirinos.unac@gmail.com>
2023-12-20 23:24:43 -0300

    Os K elementos mais Frequentes

---
+++ Simple_Python_Code/12_Top_K_Frequent_Elements.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+
+Top_K Frequent Elements
+=======================
+Dado um array inteiro nums e um inteiro k, retorne os k elementos mais frequentes.
+Você pode retornar a resposta em qualquer ordem.
+"""
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        n = len(nums)
+        
+          
+    
+
+
+
