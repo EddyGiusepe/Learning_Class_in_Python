bf6a5b90293d9bf4bf710c9fcdd56c2dc9c1e19b
EddyGiusepe <eddychirinos.unac@gmail.com>
2023-12-20 23:52:45 -0300

    Top_K --> Termos mais frequentes

--- Simple_Python_Code/12_Top_K_Frequent_Elements.py
+++ Simple_Python_Code/12_Top_K_Frequent_Elements.py
@@ -12,9 +12,29 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
         n = len(nums)
-        
-          
+        counter = Counter(nums)
+        buckets = [0] * (n + 1)
+
+        for num, freq in counter.items():
+            if buckets[freq] == 0:
+                buckets[freq] = [num]
+            else:
+                buckets[freq].append(num) 
+
+        ret = [] 
+        for i in range(n, -1, -1):
+            if buckets[i] != 0:
+                ret.extend(buckets[i])
+            if len(ret) == k:
+                break
+
+        return ret # Time O(n)            
+
+
+if __name__ == '__main__':
+    minhaClass = Solution()
+    nums = [1, 2, 3, 3, 3, 2, 4, 5, 5, 5]
+    k = 2
+    resposta = minhaClass.topKFrequent(nums=nums, k=k)
+    print(resposta)
     
-
-
-
