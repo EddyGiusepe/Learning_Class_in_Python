c05fc46514861d0dfb8536914cbc836cd8b9ec70
EddyGiusepe <eddychirinos.unac@gmail.com>
2023-12-21 23:24:29 -0300

    Uma Classe diferente em Python

---
+++ Simple_Python_Code/13_Simple_Class_Car.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+"""
+
+
+class Car:
+    def __init__(self, modelo, marca, ano):
+        self.__details__ = {
+            "modelo": modelo,
+            "marca": marca,
+            "ano": ano
+        }
+
+    def 
