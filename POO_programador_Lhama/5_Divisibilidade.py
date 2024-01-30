336a4d155a6c0b05bdaab04451db12d4bdbfd22e
EddyGiusepe <eddychirinos.unac@gmail.com>
2024-01-29 23:33:37 -0300

    Divisibilidade de um número

---
+++ POO_programador_Lhama/5_Divisibilidade.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+"""
+
+def fizzBuzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Executando:
+if __name__ == "__main__":
+    n = int(input("Digite um número inteiro positivo: "))
+    fizzBuzz(n)
+
