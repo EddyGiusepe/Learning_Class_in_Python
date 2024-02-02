c42b3c0d834fa2e3a7f48558c187897235cbfee3
EddyGiusepe <eddychirinos.unac@gmail.com>
2024-02-01 23:35:38 -0300

    Testando os TEMPOS no Locust

---
+++ locust/1_locust.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+
+Execução:
+
+$ locust -f 1_locust.py
+ou
+$ locust -f 1_locust.py --modern-ui
+"""
+from locust import HttpUser, task, constant, constant_throughput, constant_pacing, between
+import random
+from locust import TaskSet
+
+# class UsuarioQAEstresse(HttpUser):
+#     #wait_time = between(1 , 5)
+#     #wait_time = constant_throughput(10) # Me dá RPS=21. This is the mathematical inverse of constant_pacing.
+#     wait_time = constant_pacing(0.1)
+#     #wait_time = constant(0.1)  # Tempo de espera entre as solicitações em segundos (Usado no 'task').
+
+#     @task
+#     def enviar_pergunta(self):
+
+
+#         # # Enviando a query a nossa aplicação:
+#         r = self.client.get("http://0.0.0.0:8000/") # "https://google.com"
+#         print(r.status_code)
+
+
+
+class MyTaskSet(TaskSet):
+
+    @task
+    def my_task(self):
+        r = self.client.get("http://0.0.0.0:8000/")
+        print(r.status_code) 
+
+    wait_time = constant(0.1)  # Tempo entre as execuções das tarefas
+
+
+class MyUser(HttpUser):
+    tasks = [MyTaskSet]
+    min_wait = 0
+    max_wait = 0
