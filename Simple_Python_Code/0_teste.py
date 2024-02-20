6fc4a7173cb8170f668b88f4e38787cd4c9f9d68
EddyGiusepe <eddychirinos.unac@gmail.com>
2024-02-20 16:20:17 -0300

    Lembrando a sobrescrita de método numa classe

---
+++ Simple_Python_Code/0_teste.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+"""
+class Planet:
+    def __init__(self):
+        print("1")
+
+    def __init__(self):
+        print("2")
+
+
+
+planet = Planet()
