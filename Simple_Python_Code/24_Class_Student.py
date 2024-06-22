1d9da6a8db56e38807baf635c2f5ccc510e7bef3
EddyGiusepe <eddychirinos.unac@gmail.com>
2024-06-22 18:49:45 -0300

    Class simples

---
+++ Simple_Python_Code/24_Class_Student.py
@@ -0,0 +1,22 @@
+"""
+Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+"""
+
+class ProfessionalProfile:
+    # Constructor:
+    def __init__(self, name, age, degree):
+        self.name = name
+        self.age = age
+        self.degree = degree
+
+    # Method:
+    def info(self):
+        print(f"Meu nome é {self.name}, a minha idade é {self.age} e sou {self.degree} em Física.")
+
+
+
+
+if __name__ == "__main__":
+    # Instanciar o objeto:
+    professionalProfile = ProfessionalProfile("Eddy Giusepe", 42, "PhD")
+    professionalProfile.info()
