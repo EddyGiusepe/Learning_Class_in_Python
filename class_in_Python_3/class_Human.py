745665e24a3e245a6f49a512339ab08d48e87b6b
EddyGiusepe <eddychirinos.unac@gmail.com>
2024-11-14 23:26:42 -0300

    Atualizando a minha classe

--- class_in_Python_3/class_Human.py
+++ class_in_Python_3/class_Human.py
@@ -1,29 +1,37 @@
+#!/usr/bin/env python3
+"""
+Senior Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+
+Class Human
+===========
+Aqui vou praticar construindo uma classe Human.
+Lembrando os conceitos e usando outras técnicas.
+"""
 class Human:
-    def __init__(self, name, ocupation):
+    def __init__(self, name: str, occupation: str) -> None:
         self.name = name
-        self.ocupation = ocupation
+        self.occupation = occupation
 
-    def do_work(self):
-        if self.ocupation == "tennis player":
-            print(self.name, "plays tennis")
-        elif self.ocupation == "actor":
-            print(self.name, "shoots filme")
+    def work_that_does(self):
+        if self.occupation == "Senior Data Scientist":
+            print(f"{self.name}, works with Artificial Intelligence")
+        elif self.occupation == "Soccer player":
+            print(f"{self.name}, plays soccer")
+        else:
+            print(f"{self.name}, works in other field")
 
-    def speaks(self):
-        print(self.name, "says how are you?")
+    def languages_spoken(self):
+        if self.occupation == "Senior Data Scientist":
+            print(f"{self.name}, speaks Spanish and Portuguese")
+        elif self.occupation == "Soccer player":
+            print(f"{self.name}, doesn't speak English")
+
+
 
 if __name__ == "__main__":
-    tom = Human("tom Cruise", "actor")
-    tom.do_work()
-    tom.speaks()
-
+    ed = Human("Eddy", "Senior Data Scientist")
+    print(ed.name)
     print("")
-    print("Create another instance of this class: ")
-    maria = Human("maria sharapova", "tennis player")
-    maria.do_work()
-    maria.speaks()
-
-
-
-
-
+    ed.work_that_does()
+    print("")
+    ed.languages_spoken()
